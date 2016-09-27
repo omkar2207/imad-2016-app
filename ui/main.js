a05f4bb2b87ece072f221a7c1d0061028472dcd1
omkar2207 <omkargopikrishna@gmail.com>
2016-09-27 21:42:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,17 @@ console.log('Loaded!');
 // to change the text in HTML
 var element = document.getElementById ("main-text");
 element.innerHTML = "New Text";
+
+// to move the image when clicked
+
+var img = document.getElementById("image");
+
+var right=0;
+function moveRight(){
+    right = right +10;
+    img.style.marginLeft = moveRight+"px";
+}
+
+img.onClick = function(){
+setInterval(moveRight, 50);
+};
