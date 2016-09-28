aa4feb2533d436b666ca9bf9bf69ac4b36b9f3d3
omkar2207 <omkargopikrishna@gmail.com>
2016-09-29 00:08:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,13 +44,43 @@ span.innerHTML = counter.toString();
 }
 */
 
+
+
+
 var submit = document.getElementById('submit_btn');
+
 submit.onclick = function(){
-var names = ['name1','name2','name3','name4'];
-var list ='';
-for (var i=0; i<names.length; i++){
+    
+var request = new XMLHttpRequest();
+
+
+// make a request
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+request.open('GET', "http://omkar2207.imad.hasura-app.io/submit-name?name=" + name, true);
+request.send(null);
+
+
+// save the request received and span the request
+request.onreadystatechange =function(){
+ if (request.readyState === XMLHttpRequest.DONE){
+
+  if (request.status === 200){
+    var names = request.responseText;
+    names = JSON.parse(names);
+    var list ='';
+    for (var i=0; i<names.length; i++){
      list += '<li>'+ names[i] + '</li>';
-}
-var ul = document.getElementById("namelist");
-ul.innerHTML = list;
+    }
+    var ul = document.getElementById("namelist");
+    ul.innerHTML = list;
+   
+  }
+
+ }
+
+};
+
+   
+
 };
