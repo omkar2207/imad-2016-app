0d211ade4ddb3e9a687a0fe849d53838da1bffaa
omkar2207 <omkargopikrishna@gmail.com>
2016-09-28 23:05:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,3 +43,11 @@ counter =counter+1;
 span.innerHTML = counter.toString();
 }
 */
+
+var names = ['name1','name2','name3','name4'];
+list ='';
+for (var i=0, i<names.length, i++){
+     list = '<li>'+ names[i] + '</li>';
+}
+var ulList = getElementById("namelist");
+ulList.innerHTML = list;
