4edbb3fe650f03aa661a7bd19a600a20ee8fee50
omkar2207 <omkargopikrishna@gmail.com>
2016-10-01 10:54:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ return htmlcontent;
 var names =[];
 app.get('/submit-name', function(req,res){
     var name = req.query.name;
-    names.push[name];
+    names.push(name);
     res.send(JSON.stringify(names));
 });
 
