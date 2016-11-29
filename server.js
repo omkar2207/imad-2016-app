3d572189715fb0e26ec1be0fd4ab8f670590ea68
omkar2207 <omkargopikrishna@gmail.com>
2016-11-29 21:39:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('bodyParser');
 
 var config = {
   host: 'db.imad.hasura-app.io',
